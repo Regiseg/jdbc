836f4a8e8624d72d7ce70897e979854ce41954ac
Bibi <huitzilopochtlik@gmail.com>
2021-05-31 19:52:33 +0200

    JDBC Template gyakorlás

--- src/main/java/spring/EmployeeDao.java
+++ src/main/java/spring/EmployeeDao.java
@@ -1,11 +1,13 @@
 package spring;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 public class EmployeeDao {
@@ -16,17 +18,26 @@ public class EmployeeDao {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void createEmployee(String name) {
-        jdbcTemplate.update("INSERT INTO employees(emp_name) VALUES (?)", name);
+    public long createEmployee(String name) {
+        KeyHolder holder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO employees(emp_name) VALUES (?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, name);
+            return ps;
+        }, holder);
+
+        return holder.getKey().longValue();
     }
 
     public List<String> listEmployeeNames() {
         return jdbcTemplate.query("SELECT emp_name FROM employees ORDER BY emp_name",
-                new RowMapper<String>() {
-                    @Override
-                    public String mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        return rs.getString("emp_name");
-                    }
-                });
+                (rs, rowNum) -> rs.getString("emp_name"));
+    }
+
+    public String findEmployeeNameById(long id) {
+        return jdbcTemplate.queryForObject("SELECT emp_name FROM employees WHERE id=?",
+                new Object[]{id}, (rs, rowNum) -> rs.getString("emp_name"));
     }
 }
--- src/test/java/spring/EmployeeDaoTest.java
+++ src/test/java/spring/EmployeeDaoTest.java
@@ -1,10 +1,15 @@
 package spring;
 
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,12 +18,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class EmployeeDaoTest {
 
     @Autowired
+    private Flyway flyway;
+
+    @Autowired
     private EmployeeDao employeeDao;
 
-    @Test
-    void testCreateThanList(){
-        employeeDao.createEmployee("John Doe");
-
+    @BeforeEach
+    public void init() {
+        flyway.clean();
+        flyway.migrate();
     }
+
+    @Test
+    void testCreateThanList() {
+        employeeDao.createEmployee("John Doe");
+        List<String> employees = employeeDao.listEmployeeNames();
+        assertEquals(List.of("John Doe"), employees);
+
+        employeeDao.createEmployee("Bibi");
+        assertEquals(2, employeeDao.listEmployeeNames().size());
+    }
+
+    @Test
+    void testThanFind() {
+        long id = employeeDao.createEmployee("john doe");
+        String name = employeeDao.findEmployeeNameById(id);
+        assertEquals("john doe", name);
+    }
+
 
 }
